d69aebadd6dc1b4fd3acf0bb3d694dfe86767c36
Pavel Musienko <57036560+pqtech@users.noreply.github.com>
2020-08-26 13:14:03 +0300

    Add inheritance from GeneralTests class

--- src/test/java/hw4/FailTest.java
+++ src/test/java/hw4/FailTest.java
@@ -1,62 +1,24 @@
 package hw4;
 
-import hw4.pages.MainPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Stories;
 import io.qameta.allure.Story;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.testng.ITestContext;
-
-import java.io.IOException;
 
 import static hw4.steps.MainPageSteps.*;
 
-public class FailTest {
-
-    protected WebDriver driver;
-    protected MainPage mainPage;
-
-    @BeforeClass
-    public void setUp(ITestContext context) {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        // 1. Open test site by URL
-        mainPage = new MainPage(driver);
-        mainPage.openMainPage();
-        context.setAttribute("driver", driver);
-    }
+public class FailTest extends GeneralTests {
 
     @Feature(value = "Site testing")
     @Stories(value = {@Story(value = "Main page functionality"),
             @Story(value = "Service page functionality")})
     @Test(priority = 1)
+    @Override
     public void mainPageTitleBeforeLoginTest() {
         // 2. Assert Browser title
         checkMainPageTitle(mainPage);
         Assert.fail("Something went wrong...");
     }
 
-    @Feature(value = "Site testing")
-    @Stories(value = {@Story(value = "Main page functionality"),
-            @Story(value = "Service page functionality")})
-    @Test(priority = 2)
-    public void mainPageLoginTest() throws IOException {
-        // 3. Perform login
-        performLogin(mainPage);
-
-        // 4. Assert User name in the left-top side of screen that user is loggined
-        checkPageUserName(mainPage);
-    }
-
-    @AfterClass
-    public void tearDown() {
-        driver.quit();
-    }
 }
