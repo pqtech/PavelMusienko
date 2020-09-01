44ebfd72bb473e8f0b16172944a1bcb0eee0eccc
Pavel Musienko <57036560+pqtech@users.noreply.github.com>
2020-09-02 01:49:56 +0300

    Add CucumberOption for better allure report

--- src/test/java/hw5/RunAcceptanceTest.java
+++ src/test/java/hw5/RunAcceptanceTest.java
@@ -1,6 +1,8 @@
 package hw5;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
+@CucumberOptions(plugin = {"io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm"})
 public class RunAcceptanceTest extends AbstractTestNGCucumberTests {
 }
